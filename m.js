924d603bc98891d85aabff252eebe149c9571155
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-04-10 20:33:22 -0500

    Add files via upload

---
+++ m.js
@@ -0,0 +1,22 @@
+{
+	"version": "2.21.6.17.2",
+	"download": "http://apticirl.com/8E8U",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaM/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaM/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaM/main/changelog-pt.html"
+		},
+		{
+			"languaje": "pt-BR",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/WaM/main/changelog-pt.html"
+		}
+	]
+}
